7340f10a8c1be9198e19a5fde114072ffc83d7e7
Thierry Vilmart <thierry.vilmart@frog.co>
2023-01-13 17:35:33 +0100

    still compiling

--- composeios/build.gradle.kts
+++ composeios/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("com.android.library").version("7.4.0").apply(false)
     kotlin("android").version("1.7.10").apply(false)
     kotlin("multiplatform").version("1.7.10").apply(false)
+    // https://plugins.gradle.org/plugin/org.jetbrains.compose
+    id("org.jetbrains.compose") version "1.3.0-rc02"
 }
 
 tasks.register("clean", Delete::class) {
--- composeios/settings.gradle.kts
+++ composeios/settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         google()
         gradlePluginPortal()
         mavenCentral()
+        //maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
@@ -12,6 +13,26 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
+        //maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    dependencies {
+        //classpath("com.android.tools.build:gradle:7.4.0")
+        //classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
+    }
+}
+
+// https://markonovakovic.medium.com/jetpack-compose-for-ios-getting-started-step-by-step-e7be6f52edd4
+// https://gist.github.com/marenovakovic/e6c3b051922cdcc167bfe07dd4793872
+// https://gist.github.com/marenovakovic/3fd64f4c57c4f32a4b337367ec430748
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("VERSION_CATALOGS")
 
 rootProject.name = "compose-ios"
 include(":androidApp")
