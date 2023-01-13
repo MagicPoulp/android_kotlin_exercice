c0958be63ba59fa6cbd12e386e875c0dce375f8b
Thierry Vilmart <thierry.vilmart@frog.co>
2023-01-13 17:57:36 +0100

    add more UI elements

--- composeios/androidApp/src/main/java/com/example/compose_ios/android/MainActivity.kt
+++ composeios/androidApp/src/main/java/com/example/compose_ios/android/MainActivity.kt
@@ -3,11 +3,12 @@ package com.example.compose_ios.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_ios.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +29,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingView(text: String) {
-    Text(text = text)
+    MessageCard("Message")
+}
+
+@Composable
+fun MessageCard(msg: String) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = "msg.author")
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = "msg.body")
+        }
+    }
 }
 
 @Preview
