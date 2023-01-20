39852091dd04625f5494ef187d663409dd0f4c8a
Thierry Vilmart <thierry.vilmart@frog.co>
2023-01-20 15:25:42 +0100

    compy the sample for iOS and fix the kotlin version

--- composeios/build.gradle.kts
+++ composeios/build.gradle.kts
@@ -1,3 +1,6 @@
+// https://betterprogramming.pub/jetpack-compose-for-ios-getting-started-step-by-step-e7be6f52edd4
+
+/*
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("7.4.0").apply(false)
@@ -8,4 +11,128 @@ plugins {
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+*/
+
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.compose.experimental.dsl.IOSDevices
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose") version "1.3.0-rc01"
+}
+
+version = "1.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    gradlePluginPortal()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+kotlin {
+    iosX64("uikitX64") {
+        binaries {
+            executable {
+                entryPoint = "main"
+                freeCompilerArgs += listOf(
+                    "-linker-option", "-framework", "-linker-option", "Metal",
+                    "-linker-option", "-framework", "-linker-option", "CoreText",
+                    "-linker-option", "-framework", "-linker-option", "CoreGraphics"
+                )
+            }
+        }
+    }
+    iosArm64("uikitArm64") {
+        binaries {
+            executable {
+                entryPoint = "main"
+                freeCompilerArgs += listOf(
+                    "-linker-option", "-framework", "-linker-option", "Metal",
+                    "-linker-option", "-framework", "-linker-option", "CoreText",
+                    "-linker-option", "-framework", "-linker-option", "CoreGraphics"
+                )
+                // TODO: the current compose binary surprises LLVM, so disable checks for now.
+                freeCompilerArgs += "-Xdisable-phases=VerifyBitcode"
+            }
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                project(":shared") // optional. if you have KMM project
+                implementation(compose.ui)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                implementation(compose.runtime)
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {}
+        }
+
+        val nativeMain by creating {
+            dependsOn(commonMain)
+        }
+        val uikitMain by creating {
+            dependsOn(nativeMain)
+        }
+        val uikitX64Main by getting {
+            dependsOn(uikitMain)
+        }
+        val uikitArm64Main by getting {
+            dependsOn(uikitMain)
+        }
+    }
+}
+
+compose.experimental {
+    uikit.application {
+        bundleIdPrefix = "com.yourpackage"
+        projectName = "yourProjectName"
+        deployConfigurations {
+            simulator("IPhone13Pro") {
+                //Usage: ./gradlew iosDeployIPhone13ProDebug
+                device = IOSDevices.IPHONE_13_PRO
+            }
+            simulator("IPad") {
+                //Usage: ./gradlew iosDeployIPadDebug
+                device = IOSDevices.IPAD_MINI_6th_Gen
+            }
+            connectedDevice("Device") {
+                //First need specify your teamId here, or in local.properties (compose.ios.teamId=***)
+                teamId="yourReamId"
+                //Usage: ./gradlew iosDeployDeviceRelease
+            }
+        }
+    }
+    compose {
+        kotlinCompilerPlugin.set(dependencies.compiler.forKotlin("1.7.20"))
+        kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=1.7.22")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+kotlin {
+    targets.withType<KotlinNativeTarget> {
+        binaries.all {
+            // TODO: the current compose binary surprises LLVM, so disable checks for now.
+            freeCompilerArgs += "-Xdisable-phases=VerifyBitcode"
+        }
+    }
+}
+
+// TODO: remove when https://youtrack.jetbrains.com/issue/KT-50778 fixed
+project.tasks.withType(org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile::class.java).configureEach {
+    kotlinOptions.freeCompilerArgs += listOf(
+        "-Xir-dce-runtime-diagnostic=log"
+    )
 }
--- composeios/settings.gradle.kts
+++ composeios/settings.gradle.kts
@@ -1,3 +1,4 @@
+/*
 pluginManagement {
     repositories {
         google()
@@ -12,7 +13,36 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+*/
 
 rootProject.name = "compose-ios"
 include(":androidApp")
 include(":shared")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("VERSION_CATALOGS")
+
+rootProject.name = "MyProject"
