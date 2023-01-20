bd02d21261615b15fb482b40b5111707166286a9
Thierry Vilmart <thierry.vilmart@frog.co>
2023-01-20 17:26:09 +0100

    try to fix deps

--- composeios/shared/build.gradle.kts
+++ composeios/shared/build.gradle.kts
@@ -63,3 +63,13 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
 }
+
+dependencies {
+    implementation(project(":shared"))
+    implementation("androidx.compose.ui:ui:1.3.3")
+    implementation("androidx.compose.ui:ui-tooling:1.3.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.3")
+    implementation("androidx.compose.foundation:foundation:1.3.1")
+    implementation("androidx.compose.material:material:1.3.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+}
